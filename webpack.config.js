8e768f0865f12c7cd2baadcebec860ba60a6144f
Artem Tulupov <stalin.dignitas70@gmail.com>
2020-05-23 15:35:20 +0300

    fixed webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,95 +6,84 @@ const WebpackMd5Hash = require('webpack-md5-hash');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
-    entry: { 
-        main: './src/index.js' 
+    entry: {
+        main: './src/index.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[chunkhash].js'
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.js$/, // регулярное выражение, которое ищет все js файлы
-                use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
-                exclude: /node_modules/,// исключает папку node_modules
-               },
-            
-               {
+                use: {
+                    loader: "babel-loader"
+                }, // весь JS обрабатывается пакетом babel-loader
+                exclude: /node_modules/, // исключает папку node_modules
+            },
+
+            {
                 test: /\.css$/, // применять это правило только к CSS-файлам
                 use: [
-                  (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-                  'css-loader', 
-                  'postcss-loader'
+                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                    'css-loader',
+                    'postcss-loader'
                 ]
-               },
-               {
+            },
+            {
                 test: /\.(eot|svg|png|jpg|gif|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-                use: [
-                  {
+                use: [{
                     loader: "file-loader",
                     options: {
-                      outputPath: './images/',
-                      esModule: false,
+                        outputPath: './images/',
+                        esModule: false,
                     },
-                  },
-                ],
-              },
-              {
+                }, ],
+            },
+            {
                 test: /\.(ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-                use: [
-                  {
+                use: [{
                     loader: "file-loader",
                     options: {
-                      outputPath: './fonts/',
-                      esModule: false,
+                        outputPath: './fonts/',
+                        esModule: false,
                     },
-                  },
-                ],
-              },
-              {
+                }, ],
+            },
+            {
                 test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                  {
+                use: [{
                     loader: "image-webpack-loader",
                     options: {
-                      name: "[path][chunkhash].[ext]",
-                      bypassOnDebug: true,
-                      disable: false,
+                        name: "[path][chunkhash].[ext]",
+                        bypassOnDebug: true,
+                        disable: false,
                     },
-                  },
-                ],
-              },
-            ],
-          },
-           
-        plugins: [
-          new MiniCssExtractPlugin({
-            filename: "./[chunkhash].css",
-          }),
-          new OptimizeCssAssetsPlugin({
+                }, ],
+            },
+        ],
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./index.[chunkhash].css",
+        }),
+        new OptimizeCssAssetsPlugin({
             assetNameRegExp: /\.css$/g,
             cssProcessor: require("cssnano"),
             cssProcessorPluginOptions: {
-              preset: ["default"],
+                preset: ["default"],
             },
             canPrint: true,
-          }),
-          new HtmlWebpackPlugin({
+        }),
+        new HtmlWebpackPlugin({
             inject: false,
             template: "./src/index.html",
             filename: "index.html",
-          }),
-          new WebpackMd5Hash(),
-          new webpack.DefinePlugin({
+        }),
+        new WebpackMd5Hash(),
+        new webpack.DefinePlugin({
             NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-          }),
-        ],
+        }),
+    ],
 }
-
-
-
-
-   
-   
